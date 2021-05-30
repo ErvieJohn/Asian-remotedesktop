0be027b6d5a0c4b8ea2e2c0891c1a8b444f268e8
Ervie John T. Villareal <53289837+ErvieJohn@users.noreply.github.com>
2021-05-30 14:06:02 +0800

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Azure ni PHC-Zaxcal" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure ni Ervs [credits: PHC-Zaxcal]" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user administrator Ervs@20 /add >nul
 net localgroup administrators administrator /add >nul
@@ -10,7 +10,7 @@ tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -
 echo User: Administrator
 echo Pass: Ervs@20
 curl -O https://raw.githubusercontent.com/Shawn2k/Asian-remotedesktop/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Shawn2k/Asian-remotedesktop/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/ErvieJohn/Asian-remotedesktop/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
